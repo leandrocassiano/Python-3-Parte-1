3f1f7f394edd34d040d0a44c08cbfb544ab4b204
Leandro Cassiano <leandrocassiano@outlook.com>
2022-01-30 17:59:20 -0300

    Utilizando o laço de repetição While em Python.

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,19 +5,31 @@ print("**********************************")
 numero_secreto = 42
 
 chute_str = input("Digite o seu numero: ")
-
 chute = int(chute_str)
-
 print("Você digitou", chute_str)
+total_de_tentativas = 3
+rodada = 1
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-if(acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif (menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        rodada = rodada + 4
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
